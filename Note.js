3c4da447d062d87f2e0a215328edf8009273aa25
ShahLucky444 <shahlucky755@gmail.com>
2026-06-12 20:38:55 +0530

    javascrip for note app

---
+++ Note.js
@@ -0,0 +1,8 @@
+let addbtn = document.getElementById("addbtn");
+
+let addnew = document.querySelector(".addnew");
+let note = document.querySelector(".note");
+
+addnew.addEventListener("click", () => {
+  note.style.display = "block";
+});
